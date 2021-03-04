f3f509efb165712bac4a41aed5149548a53c53ce
richardchien <richardchienthebest@gmail.com>
2021-03-04 12:02:35 +0000

    Deploying to gh-pages from  @ 3496be9d0c3f090ceb0a99fcdd8bcb93750909dc 🚀

--- implementors/actix_http/httpmessage/trait.HttpMessage.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {};
-implementors["actix_http"] = [];
-implementors["awc"] = [{"text":"impl&lt;S&gt; HttpMessage for ClientResponse&lt;S&gt;","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
